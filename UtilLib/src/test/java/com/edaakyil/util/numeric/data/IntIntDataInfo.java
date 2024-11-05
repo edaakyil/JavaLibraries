c035c6ce6aa1e05d8d13edc7ff08a5ad97ab29e7
Eda Akyil <edaakyil@gmail.com>
2024-11-05 16:08:33 +0300

    IntIntDataInfo.java is created

---
+++ UtilLib/src/test/java/com/edaakyil/util/numeric/data/IntIntDataInfo.java
@@ -0,0 +1,19 @@
+/**
+ * Author: Eda Akyıl
+ * User:edaakyil
+ * Date:5.11.2024
+ * Time:16:02
+ */
+
+package com.edaakyil.util.numeric.data;
+
+public class IntIntDataInfo {
+    public int input;
+    public int expected;
+
+    public IntIntDataInfo(int input, int expected)
+    {
+        this.input = input;
+        this.expected = expected;
+    }
+}
