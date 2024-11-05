4cef4f3b261356ef7308236b83867ab2d5ba5972
Eda Akyil <edaakyil@gmail.com>
2024-11-05 15:24:23 +0300

    NumberUtilFibonacciTest.java is updated

--- UtilLib/src/test/java/com/edaakyil/util/numeric/NumberUtilFibonacciTest.java
+++ UtilLib/src/test/java/com/edaakyil/util/numeric/NumberUtilFibonacciTest.java
@@ -3,15 +3,41 @@ package com.edaakyil.util.numeric;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Collection;
+import java.util.List;
 
 @Ignore("Tested before and passed")
+@RunWith(Parameterized.class)
 public class NumberUtilFibonacciTest {
+    DataInfo dataInfo;
+
+    public NumberUtilFibonacciTest(DataInfo info)
+    {
+        dataInfo = info;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<DataInfo> cerateData()
+    {
+        return List.of(new DataInfo(1, 0), new DataInfo(2, 1), new DataInfo(3, 1), new DataInfo(4, 2), new DataInfo(5, 3));
+    }
+
+    static class DataInfo {
+        int input;
+        int expected;
+
+            DataInfo(int a, int b)
+            {
+                input = a;
+                expected = b;
+            }
+    }
     @Test
     public void givenValue_WhenIndex_ThenReturnsNumber()
     {
-        int expected = 3;
-        int input = 5;
-
-        Assert.assertEquals(expected, NumberUtil.fibonacciNumber(input));
+        Assert.assertEquals(dataInfo.expected, NumberUtil.fibonacciNumber(dataInfo.input));
     }
 }
